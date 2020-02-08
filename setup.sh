23167cc0f96ff0c565f992eccd9834bd3bdfa492
Aleksandr Sabinin <>
2020-02-08 10:49:30 +0100

    Added script to automate configuration

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+
+ln -sf ${SCRIPT_DIR}/.emacs ${HOME}/
+
+EMACS_DIR='.emacs.d'
+rm -rf ${HOME}/${EMACS_DIR}
+ln -sf ${SCRIPT_DIR}/${EMACS_DIR} ${HOME}/
+
+sudo apt install \
+     emacs \
+     git \
+     ttf-anonymous-pro
+
+emacs --eval "(package-install-selected-packages)"
