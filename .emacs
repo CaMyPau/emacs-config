2cb454f30d1b161c9a5ed65ef7839fda7d91d7dd
Aleksandr Sabinin <alexander.sabinin@gmail.com>
2021-02-09 16:06:59 +0100

    Multiple changes:
    - Greatly simplified ediff window configuration managment
    - Removed GUD source buffer reuse fix
    - Introduced smart-tabs mode (indent with tabs, align with spaces)

--- .emacs
+++ .emacs
@@ -1,12 +1,12 @@
 ;; .emacs
-;; (set-face-font 'default "Anonymous Pro-16")
+;; (set-face-font 'default "Anonymous Pro-32")
 ;; (set-default-font "Anonymous Pro-16")
 ;; (set-face-font 'default "Anonymous Pro-16")
 
 (setq default-frame-alist '((font . "Anonymous Pro-16")))
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -21,6 +21,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(auth-source-save-behavior nil)
  '(backup-directory-alist '((".*" . "~/.emacs.d/.backup/")))
  '(before-save-hook '(whitespace-cleanup))
  '(c-basic-offset 4)
@@ -34,7 +35,7 @@
  '(cursor-type 'bar)
  '(custom-enabled-themes '(monokai))
  '(custom-safe-themes
-   '("9abe2b502db3ed511fea7ab84b62096ba15a3a71cdb106fd989afa179ff8ab8d" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "d3a406c5905923546d8a3ad0164a266deaf451856eca5f21b36594ffcb08413a" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default))
+   '("d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "8b58ef2d23b6d164988a607ee153fd2fa35ee33efc394281b1028c2797ddeebb" "9abe2b502db3ed511fea7ab84b62096ba15a3a71cdb106fd989afa179ff8ab8d" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "d3a406c5905923546d8a3ad0164a266deaf451856eca5f21b36594ffcb08413a" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default))
  '(delete-by-moving-to-trash t)
  '(delete-selection-mode t)
  '(desktop-path '("~/.emacs.d/"))
@@ -67,12 +68,13 @@
  '(history-length t)
  '(indent-tabs-mode nil)
  '(magit-diff-use-overlays nil)
+ '(magit-git-executable "/opt/bb/bin/git")
  '(menu-bar-mode nil)
  '(mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control))))
  '(nrepl-message-colors
    '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(package-selected-packages
-   '(keychain-environment goto-last-change flycheck-rust markdown-mode monokai-theme shell-history magit company-c-headers company racer toml-mode cargo rust-mode go-mode flycheck-pyflakes))
+   '(smart-tabs-mode dockerfile-mode yaml-mode keychain-environment goto-last-change flycheck-rust markdown-mode monokai-theme shell-history magit company-c-headers company racer toml-mode cargo rust-mode go-mode flycheck-pyflakes))
  '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(pos-tip-background-color "#A6E22E")
  '(pos-tip-foreground-color "#272822")
@@ -119,7 +121,7 @@
  '(weechat-color-list
    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
  '(whitespace-style
-   '(face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark newline-mark))
+   '(face trailing tabs spaces newline empty indentation space-before-tab space-mark tab-mark newline-mark))
  '(woman-fill-column 170)
  '(woman-use-own-frame nil))
 
@@ -174,3 +176,7 @@
 ;; Add color formatting to *compilation* buffer
 (add-hook 'compilation-filter-hook
   (lambda () (ansi-color-apply-on-region (point-min) (point-max))))
+
+(setq-default gdb-display-io-nopopup t)
+
+(smart-tabs-insinuate 'c++)
--- .emacs.d/lisp/memory.el
+++ .emacs.d/lisp/memory.el
@@ -125,40 +125,20 @@ by using nxml's indentation rules."
           (kill-buffer buffer)))
       (message "Killed %i dired buffer(s)." count ))))
 
-;; Some custom configuration to ediff
-(defvar my-ediff-bwin-config nil "Window configuration before ediff.")
-(defcustom my-ediff-bwin-reg ?b
-  "*Register to be set up to hold `my-ediff-bwin-config'
-    configuration.")
 
-(defvar my-ediff-awin-config nil "Window configuration after ediff.")
-(defcustom my-ediff-awin-reg ?e
-  "*Register to be used to hold `my-ediff-awin-config' window
-    configuration.")
 
-(defun my-ediff-bsh ()
-  "Function to be called before any buffers or window setup for
-    ediff."
-  (setq my-ediff-bwin-config (current-window-configuration))
-  (when (characterp my-ediff-bwin-reg)
-    (set-register my-ediff-bwin-reg
-                  (list my-ediff-bwin-config (point-marker)))))
+(defvar my-ediff-last-windows nil)
 
-(defun my-ediff-ash ()
-  "Function to be called after buffers and window setup for ediff."
-  (setq my-ediff-awin-config (current-window-configuration))
-  (when (characterp my-ediff-awin-reg)
-    (set-register my-ediff-awin-reg
-                  (list my-ediff-awin-config (point-marker)))))
+(defun my-store-pre-ediff-winconfig ()
+  (setq my-ediff-last-windows (current-window-configuration)))
 
-(defun my-ediff-qh ()
-  "Function to be called when ediff quits."
-  (when my-ediff-bwin-config
-    (set-window-configuration my-ediff-bwin-config)))
+(defun my-restore-pre-ediff-winconfig ()
+  (set-window-configuration my-ediff-last-windows))
 
-(add-hook 'ediff-before-setup-hook        'my-ediff-bsh        )
-(add-hook 'ediff-after-setup-windows-hook 'my-ediff-ash 'append)
-(add-hook 'ediff-quit-hook                'my-ediff-qh         )
+(add-hook 'ediff-before-setup-hook #'my-store-pre-ediff-winconfig)
+(add-hook 'ediff-quit-hook #'my-restore-pre-ediff-winconfig)
+
+
 
 (defun my-reverse-region (&optional arg)
   "Reverse current region, like this: \"a(bc) d\" -> \"d )cb(a\"."
@@ -188,55 +168,15 @@ by using nxml's indentation rules."
 
 (defun my-indent-tabs-mode()
   (interactive)
-  (setq indent-tabs-mode t))
+  (setq indent-tabs-mode t)
+  (setq smart-tabs-mode t)
+  (setq before-save-hook nil))
 
 (defun my-indent-spaces-mode()
   (interactive)
-  (setq indent-tabs-mode nil))
+  (setq smart-tabs-mode nil)
+  (setq indent-tabs-mode nil)
+  (add-hook 'before-save-hook 'whitespace-cleanup))
 
-; Fix to gud to force it to reuse the same code buffer
-(defadvice gud-display-line (around do-it-better activate)
-  (let* ((last-nonmenu-event t)      ; Prevent use of dialog box for questions.
-     (buffer
-      (with-current-buffer gud-comint-buffer
-        (gud-find-file true-file)))
-     (window (and buffer
-                  (or (if (eq gud-minor-mode 'gdbmi)
-                          (unless (gdb-display-source-buffer buffer)
-                            (gdb-display-buffer buffer nil 'visible)))
-                      (get-buffer-window buffer)
-                      (display-buffer buffer))))
-     (pos))
-    (when buffer
-      (with-current-buffer buffer
-    (unless (or (verify-visited-file-modtime buffer) gud-keep-buffer)
-      (if (yes-or-no-p
-           (format "File %s changed on disk.  Reread from disk? "
-               (buffer-name)))
-          (revert-buffer t t)
-        (setq gud-keep-buffer t)))
-    (save-restriction
-      (widen)
-      (goto-char (point-min))
-      (forward-line (1- line))
-      (setq pos (point))
-      (or gud-overlay-arrow-position
-          (setq gud-overlay-arrow-position (make-marker)))
-      (set-marker gud-overlay-arrow-position (point) (current-buffer))
-      ;; If they turned on hl-line, move the hl-line highlight to
-      ;; the arrow's line.
-      (when (featurep 'hl-line)
-        (cond
-         (global-hl-line-mode
-          (global-hl-line-highlight))
-         ((and hl-line-mode hl-line-sticky-flag)
-          (hl-line-highlight)))))
-    (cond ((or (< pos (point-min)) (> pos (point-max)))
-           (widen)
-           (goto-char pos))))
-      (when window
-    (set-window-point window gud-overlay-arrow-position)
-    (if (eq gud-minor-mode 'gdbmi)
-        (setq gdb-source-window window))))))
 
 (provide 'memory)
